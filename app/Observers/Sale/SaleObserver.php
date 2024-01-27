4e9676e1eca487613d606992346fe61c00ab8959
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-01-26 23:12:03 -0300

    removed unused methods on SaleObserver

--- app/Observers/Sale/SaleObserver.php
+++ app/Observers/Sale/SaleObserver.php
@@ -8,50 +8,10 @@ use App\Models\Sale\Sale;
 class SaleObserver
 {
     /**
-     * Handle the Sale "created" event.
-     */
-    public function created(Sale $sale): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Sale "updated" event.
-     */
-    public function updated(Sale $sale): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Sale "deleted" event.
+     * Handle the Sale "deleting" event.
      */
     public function deleting(Sale $sale): void
     {
         $sale->products()->detach();
-    }
-
-    /**
-     * Handle the Sale "deleted" event.
-     */
-    public function deleted(Sale $sale): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Sale "restored" event.
-     */
-    public function restored(Sale $sale): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Sale "force deleted" event.
-     */
-    public function forceDeleted(Sale $sale): void
-    {
-        //
     }
 }
