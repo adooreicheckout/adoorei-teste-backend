c2db987232cac03b80d8847caff9215d25fb9c8c
Leandro Nascimento <lfsnascimento84@gmail.com>
2024-02-29 15:44:27 -0300

    Fix scale

--- database/migrations/2024_02_28_192400_create_products_table.php
+++ database/migrations/2024_02_28_192400_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedDecimal('price', $precision = 8, $scale = 2)->nullable(false);
+            $table->unsignedDecimal('price', $precision = 8, $scale = 3)->nullable(false);
             $table->string('description');
             $table->timestamps();
             $table->softDeletes();
