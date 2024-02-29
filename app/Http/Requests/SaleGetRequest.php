b077f5f0c59d861ce3bd34bb8e124e144b5b6116
Leandro Nascimento <lfsnascimento84@gmail.com>
2024-02-29 16:07:29 -0300

    Create endpoint to get sale by id

---
+++ app/Http/Requests/SaleGetRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+
+class SaleGetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sales_id' => 'required|exists:sales,sales_id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'sales_id.required' => 'The "sale_id" is required!',
+            'sales_id.exists' => 'The sale not exists in database!',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $exception = new ValidationException($validator);
+
+        throw $exception;
+    }
+}
