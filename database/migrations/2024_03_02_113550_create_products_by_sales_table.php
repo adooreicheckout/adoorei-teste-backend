8b0792157e5a1d225ebe3db602b355d8904ac541
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-03-02 09:58:49 -0300

    migrations

---
+++ database/migrations/2024_03_02_113550_create_products_by_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_by_sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->foreignId('sale_id')->references('id')->on('sales');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products_by_sales');
+    }
+};
