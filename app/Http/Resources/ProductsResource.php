ef14c2879d72442d6d2752a42b0e89f5705fd175
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-03-03 19:01:23 -0300

    Adding ID to resource

--- app/Http/Resources/ProductsResource.php
+++ app/Http/Resources/ProductsResource.php
@@ -15,6 +15,7 @@ class ProductsResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'price' => $this->price,
             'description' => $this->description,
