2c152c657dfb72baf47261ab9506b2750de915c9
Junior Nunes <nunes.junior@gmail.com>
2024-03-03 15:12:21 -0300

    Mysql Configuration

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
